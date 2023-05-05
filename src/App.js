2da38d01e90d654b9c0ebe556f48a96162ab3bcf
Pat Gao <patgao@Camerons-MacBook-Pro.local>
2023-05-05 17:29:28 -0600

    loading page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import flower from './flower.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={flower} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This project is a work in progress.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
+      <a href="https://www.flaticon.com/free-icons/flower" title="flower icons">Flower icon created by Smashicons - Flaticon</a>
     </div>
   );
 }
