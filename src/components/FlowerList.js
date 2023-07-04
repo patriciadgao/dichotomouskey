a16f8b40dbdf65d598b67291eea1e0a9f1625bf3
patriciadgao <patriciadgao@gmail.com>
2023-07-03 23:12:07 -0600

    persist sort style

--- src/components/FlowerList.js
+++ src/components/FlowerList.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { flowers } from "../flowers/flowers";
 import { Chip } from "./Chip";
 import { FlowerListItem } from "./FlowerListItem";
@@ -9,17 +9,24 @@ const flowersAlphabetizedByGenus = [...flowers].sort((a, b) => a.genus.localeCom
 const reversedFlowers = [...flowers].reverse();
 
 export function FlowerList(props) {
-    const [sortStyle, setSortStyle] = useState('time');
+    const [sortStyle, setSortStyle] = useState(
+        localStorage.getItem('sort-style') ?? 'time'
+    );
     const [descriptionsHidden, setDescriptionsHidden] = useState(false);
     const { showModal, hideModal, modalOpen } = props;
     const [selectedFlower, setSelectedFlower] = useState(undefined);
 
+    const changeSortStyle = useCallback((sortStyle) => {
+        setSortStyle(sortStyle);
+        localStorage.setItem('sort-style', sortStyle);
+    }, []);
+
     return (
         <div>
             <div className="flex justify-center mt-6">
-                <Chip active={sortStyle === 'time'} onClick={() => setSortStyle('time')}>sort{sortStyle === 'time' ? "ed" : ""} chronologically</Chip>
-                <Chip active={sortStyle === 'name'} onClick={() => setSortStyle('name')}>sort{sortStyle === 'name' ? "ed" : ""} by name</Chip>
-                <Chip active={sortStyle === 'genus'} onClick={() => setSortStyle('genus')}>sort{sortStyle === 'genus' ? "ed" : ""} by genus</Chip>
+                <Chip active={sortStyle === 'time'} onClick={() => changeSortStyle('time')}>sort{sortStyle === 'time' ? "ed" : ""} chronologically</Chip>
+                <Chip active={sortStyle === 'name'} onClick={() => changeSortStyle('name')}>sort{sortStyle === 'name' ? "ed" : ""} by name</Chip>
+                <Chip active={sortStyle === 'genus'} onClick={() => changeSortStyle('genus')}>sort{sortStyle === 'genus' ? "ed" : ""} by genus</Chip>
             </div>
             <div className="px-4 sm:px-8 flex justify-end mt-2 space-x-2 items-center hover:cursor-pointer max-w-[750px] m-auto" onClick={() => setDescriptionsHidden(!descriptionsHidden)}>
                 <input
