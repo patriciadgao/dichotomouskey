f523a5860708aad47dcc5b1f7b48ebc75bff4eb2
patriciadgao <patriciadgao@gmail.com>
2023-08-06 15:48:30 -0600

    dots for genuses with new flowers

--- src/components/FlowerGenusBunch.js
+++ src/components/FlowerGenusBunch.js
@@ -11,14 +11,16 @@ export function FlowerGenusBunch(props) {
 
     useEffect(() => setOpen(initialOpenStatus), [initialOpenStatus]);
 
-    const genusBubbleClassName = `px-1 min-w-[16px] text-xs font-bold rounded-full text-white ${flowers.some((f) => f.new) ? 'bg-pink-400' : 'bg-lime-500'}`;
+    const newFlowers = flowers.filter((f) => f.new);
+    const genusNumberClassName = `px-1 min-w-[16px] text-xs font-bold rounded-full text-white ${newFlowers.length > 0 ? 'bg-pink-400' : 'bg-lime-500'}`
 
     return (
         <div>
             <div className="px-2 flex justify-between py-2 space-x-2 items-center hover:cursor-pointer max-w-[750px] m-auto" style={{ backgroundColor: getLightHexCode(bgColor) }}>
                 <div className="flex space-x-1.5 items-center">
                     <div>{genus}</div>
-                    <div className={genusBubbleClassName}>{numFlowers}</div>
+                    <div className={genusNumberClassName}>{numFlowers}</div>
+                    <div className="flex items-center space-x-1">{newFlowers.map((f) => <div className="bg-pink-400 w-2 h-2 rounded-full" />)}</div>
                 </div>
                 <div onClick={() => setOpen(!open)} className="grow flex justify-end">
                     <FontAwesomeIcon icon={open ? faChevronDown : faChevronRight} />
