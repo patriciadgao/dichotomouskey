4f13e60fb73f45d99cc8119d0f2fdb6d53305d54
patriciadgao <patriciadgao@gmail.com>
2024-06-24 16:39:17 -0600

    alphabetical listing

--- src/components/FlowerBunch.js
+++ src/components/FlowerBunch.js
@@ -4,22 +4,22 @@ import { useEffect, useState } from "react";
 import { getLightHexCode } from "./Colorblock";
 import { FlowerListItem } from "./FlowerListItem";
 
-export function FlowerGenusBunch(props) {
-    const { genus, flowers, viewFlower, initialOpenStatus, descriptionsHidden, bgColor } = props;
+export function FlowerBunch(props) {
+    const { bunchName, flowers, viewFlower, initialOpenStatus, descriptionsHidden, bgColor } = props;
     const [open, setOpen] = useState(true);
     const numFlowers = flowers.length;
 
     useEffect(() => setOpen(initialOpenStatus), [initialOpenStatus]);
 
     const newFlowers = flowers.filter((f) => f.new);
-    const genusNumberClassName = `px-1 min-w-[16px] text-xs font-bold rounded-full text-white ${newFlowers.length > 0 ? 'bg-pink-400' : 'bg-lime-500'}`
+    const numberClassName = `px-1 min-w-[16px] text-xs font-bold rounded-full text-white ${newFlowers.length > 0 ? 'bg-pink-400' : 'bg-lime-500'}`
 
     return (
         <div>
             <div className="px-2 flex justify-between py-2 space-x-2 items-center hover:cursor-pointer max-w-[750px] m-auto" style={{ backgroundColor: getLightHexCode(bgColor) }}>
                 <div className="flex space-x-1.5 items-center">
-                    <div>{genus}</div>
-                    <div className={genusNumberClassName}>{numFlowers}</div>
+                    <div>{bunchName}</div>
+                    <div className={numberClassName}>{numFlowers}</div>
                     <div className="flex items-center space-x-1">{newFlowers.map((f) => <div className="bg-pink-400 w-2 h-2 rounded-full" />)}</div>
                 </div>
                 <div onClick={() => setOpen(!open)} className="grow flex justify-end">
--- src/components/FlowerList.js
+++ src/components/FlowerList.js
@@ -1,16 +1,19 @@
+import capitalize from "lodash.capitalize";
 import groupBy from "lodash.groupby";
 import { useCallback, useMemo, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { flowers } from "../flowers/flowers";
 import { Chip } from "./Chip";
-import { FlowerGenusBunch } from "./FlowerGenusBunch";
+import { FlowerBunch } from "./FlowerBunch";
 import { FlowerListItem } from "./FlowerListItem";
 import { FlowerModal } from "./FlowerModal";
 
 const alphabetizedFlowers = [...flowers].sort((a, b) => a.name.localeCompare(b.name));
 const reversedFlowers = [...flowers].reverse();
 const flowersGroupedByGenus = groupBy(flowers, 'genus');
-const sortedGenusFlowers = Object.entries(flowersGroupedByGenus).sort((a, b) => a[0].localeCompare(b[0]))
+const sortedGenusFlowers = Object.entries(flowersGroupedByGenus).sort((a, b) => a[0].localeCompare(b[0]));
+const flowersGroupedByFirstLetter = groupBy(alphabetizedFlowers, (f) => capitalize(f.name[0]));
+const sortedFirstLetterFlowers = Object.entries(flowersGroupedByFirstLetter).sort((a, b) => a[0].localeCompare(b[0]))
 
 export function FlowerList(props) {
     const { bgColor } = props;
@@ -27,7 +30,7 @@ export function FlowerList(props) {
     }, [flowerName]);
 
     const [descriptionsHidden, setDescriptionsHidden] = useState(false);
-    const [openGenus, setOpenGenus] = useState(true);
+    const [openBunches, setOpenBunches] = useState(true);
 
     const changeSortStyle = useCallback((sortStyle) => {
         setSortStyle(sortStyle);
@@ -54,43 +57,45 @@ export function FlowerList(props) {
                     className="w-5 h-5 checked:text-pink-300 outline-0 border-none rounded-sm focus:ring-0 shadow"
                 /><div>hide descriptions</div>
             </div>
-            {sortStyle === 'genus' && (
-                <div className="px-4 sm:px-8 flex justify-end mt-2 space-x-2 items-center hover:cursor-pointer max-w-[750px] m-auto" onClick={() => setOpenGenus(!openGenus)}>
+            {sortStyle !== 'time' && (
+                <div className="px-4 sm:px-8 flex justify-end mt-2 space-x-2 items-center hover:cursor-pointer max-w-[750px] m-auto" onClick={() => setOpenBunches(!openBunches)}>
                     <input
                         type="checkbox"
-                        checked={openGenus}
+                        checked={openBunches}
                         readOnly
                         className="w-5 h-5 checked:text-pink-300 outline-0 border-none rounded-sm focus:ring-0 shadow"
                     /><div>expand all</div>
                 </div>
             )}
-            {sortStyle === 'name' ? (<div className="sm:px-8 pb-5 pt-3 divide-y-2">
-                {alphabetizedFlowers.map((flower, i) => (
+            {sortStyle === 'name' ? (<div className="pt-3 pb-5 sm:px-8">{sortedFirstLetterFlowers.map((firstLetter, i) => (
+                <FlowerBunch
+                    key={i}
+                    bgColor={bgColor}
+                    bunchName={firstLetter[0]}
+                    flowers={firstLetter[1]}
+                    viewFlower={viewFlower}
+                    initialOpenStatus={openBunches}
+                    descriptionsHidden={descriptionsHidden}
+                />
+            ))}</div>) : sortStyle === 'time' ? <div className="sm:px-8 pb-5 pt-3 divide-y-2">
+                {reversedFlowers.map((flower, i) => (
                     <FlowerListItem
                         flower={flower}
                         key={i}
                         hideDescription={descriptionsHidden}
                         onClick={() => viewFlower(flower)}
                     />
-                ))}</div>) : sortStyle === 'time' ? <div className="sm:px-8 pb-5 pt-3 divide-y-2">
-                    {reversedFlowers.map((flower, i) => (
-                        <FlowerListItem
-                            flower={flower}
-                            key={i}
-                            hideDescription={descriptionsHidden}
-                            onClick={() => viewFlower(flower)}
-                        />
-                    ))}</div> : <div className="pt-3 pb-5 sm:px-8">{sortedGenusFlowers.map((genus, i) => (
-                        <FlowerGenusBunch
-                            key={i}
-                            bgColor={bgColor}
-                            genus={genus[0]}
-                            flowers={genus[1]}
-                            viewFlower={viewFlower}
-                            initialOpenStatus={openGenus}
-                            descriptionsHidden={descriptionsHidden}
-                        />
-                    ))}</div>}
+                ))}</div> : <div className="pt-3 pb-5 sm:px-8">{sortedGenusFlowers.map((genus, i) => (
+                    <FlowerBunch
+                        key={i}
+                        bgColor={bgColor}
+                        bunchName={genus[0]}
+                        flowers={genus[1]}
+                        viewFlower={viewFlower}
+                        initialOpenStatus={openBunches}
+                        descriptionsHidden={descriptionsHidden}
+                    />
+                ))}</div>}
             <FlowerModal selectedFlower={flower} />
         </div>
     )
